1681797b4e56a2e913de5919dad8577a73a9815e
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-03 15:07:32 -0300

    get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
 import { Post, Put } from '@nestjs/common/decorators';
+import { randomUUID } from 'crypto';
 
 @Controller('app')
 export class AppController {
   constructor(private prisma: PrismaService) {}
 
   @Get('hello')
-  async getHello(): Promise<string> {
-    const user = await this.prisma.User.create({
+  async getHello(): Promise<object> {
+    const user = await this.prisma.Usuario.create({
       data: {
-        id: '1',
+        id: randomUUID(),
         name: 'João',
         function: 'Codar',
-      }
+      },
+    })
+    return user;
+  }
+
+  @Get('hello2')
+  async getHello2(): Promise<object> {
+    const user = await this.prisma.Usuario.create({
+      data: {
+        id: randomUUID(),
+        name: 'Marcelo',
+        function: 'Dormir',
+      },
     })
     return user;
   }
